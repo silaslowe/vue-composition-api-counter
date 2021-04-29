6c7f4c40081ab79ba5fa84d7e16391a52f7bbed2
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-29 18:26:53 -0500

    iniitial

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import {reactive, readonly} from 'vue'
+
+const state = reactive({
+    counter: 0,
+    colorCode: 'red'
+})
+
+const methods = {
+    increaseCounter() {
+        state.counter++
+    },
+    decreaseCounter() {
+        state.counter--
+    },
+    setColorCode(val) {
+        state.colorCode = val
+    }
+}
+
+const getters = {
+    counterSquared() {
+        return state.counter * state.counter
+    }
+}
+
+export default {
+    state: readonly(state),
+    methods,
+    getters
+}
